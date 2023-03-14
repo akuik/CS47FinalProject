888b74f40d3bc6de52de3101201c7e260a514eaa
Andrew Kuik <akuik@stanford.edu>
2023-03-13 22:11:02 -0700

    Removed conflicts

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,20 +1,29 @@
-import { StyleSheet, Text, View, Button, SafeAreaView, TouchableOpacity } from 'react-native'
-import React from 'react'
-import tw from 'tailwind-react-native-classnames'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import tw from "tailwind-react-native-classnames";
 
 const ProfileScreen = () => {
   return (
     <View style={tw`items-center`}>
+      <Text style={tw`text-3xl font-bold`}>Profile</Text>
       <Text style={tw`font-bold`}>Andrew Kuik</Text>
       <Text>Hi I am Andrew, I enjoy...</Text>
+      {/* Create a template for a profile */}
       <TouchableOpacity style={styles.appButtonContainer}>
-        <Text style={styles.appButtonText}>Add Friend</Text>
+        <Text style={styles.appButtonText}>Edit Profile</Text>
       </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default ProfileScreen
+export default ProfileScreen;
 
 const styles = StyleSheet.create({
   appButtonContainer: {
@@ -22,13 +31,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#009688",
     borderRadius: 10,
     paddingVertical: 10,
-    paddingHorizontal: 12
+    paddingHorizontal: 12,
   },
   appButtonText: {
     fontSize: 18,
     color: "#fff",
     fontWeight: "bold",
     alignSelf: "center",
-    textTransform: "uppercase"
-  }
-})
+    textTransform: "uppercase",
+  },
+});
